891b913553d212b98ede083fbf0fc824397dcd86
Prateek Gupta <prateekgupta.3991@gmail.com>
2016-12-24 00:41:25 +0530

    Integrated ui-router api

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var app = angular.module('StayUpdated',[]);
+var app = angular.module('StayUpdated',['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider.state('home',
+			{
+			url:'/home',
+			templateUrl:'/home.html',
+			controller:'MainCtrl'
+		});
+		$urlRouterProvider.otherwise('home');
+	}
+]);
 
 app.factory('userPost',[function() {
 		var o = {
